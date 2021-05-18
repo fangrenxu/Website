0146d0682f3810fcd8b5b70b2f793325e8666205
方人胥 <37834565+fangrenxu@users.noreply.github.com>
2021-05-18 15:18:48 +0800

    已经登录的验证

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,7 +3,3 @@ O
 数据库的结构和数据.sql,6\5\651fa22cf8d31850af5af392c70b70f9346572a6
 B
 lesson1/cookie.php,e\1\e185be9e19fd76175324072285b47592708f5847
-G
-coures2/inc/connect.php,1\2\12a3f29fbb309a72fade4d7ad7cbb702a30e1573
-A
-lesson1/check.php,4\e\4e495830356f884ef9b8fc78c50e65bdd50401d9
--- lesson1/check.php
+++ lesson1/check.php
@@ -1,1 +1,43 @@
 <?php
+//  开启会话
+session_start(); // 向cokkie中写入session_id：PHPSESSID
+
+
+if (isset($_SESSION['username']) && $_SESSION['username'] === 'admin') {
+    echo '<script>alert(\'您已经登陆了，请不要重复登陆\'); location.assign("admin.php);</script>';
+
+}
+
+
+
+
+
+
+//  连接数据库
+require __DIR__. '/inc/connect.php';
+
+$email = $_POST['email'];
+$password = sha1($_POST['password']);
+
+//  用user表中进行验证
+$sql = 'SELECT * FROM `user` WHERE `email`=:email AND `password` =:password LIMIT 1';
+
+// 创建一个SQL语句模板的对象：预处理对象
+$stmt = $pdo->prepare($sql);
+//  执行
+$stmt->execute(['email'=>$email,'password'=>$password]);
+
+
+//  fetch()
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+//print_r($user); die;
+if (false === $user){
+    // 登录失败
+    echo '<script>alert("登录失败，请检查"); location.href="login.php";</script>';
+    die();  //exit; exit();
+}
+
+//  登录成功，将用户登录信息写到cookie中
+$_SESSION['username'] = $user['username'];
+
+echo '<script>alert(\'登录成功\'); location.assign("admin.php");</script>';
