140feef6088b6e7df7c33fbc870b288c36761a73
方人胥 <37834565+fangrenxu@users.noreply.github.com>
2021-05-19 14:17:27 +0800

    用cookie的方法做用户登陆与验证

--- lesson1/login2.php
+++ lesson1/login2.php
@@ -5,7 +5,32 @@
     <title>用户登录</title>
 </head>
 <body>
+<h3>用户登陆</h3>
+<form action="check2.php" method="post" onsubmit="return isEmpty();">
+    <p>
+        <label for="email">邮箱：</label>
+        <input type="email" name="email" id="email">
+    </p>
 
+    <p>
+        <label for="password">密码：</label>
+        <input type="password" name="password" id="password">
+    </p>
+
+    <p>
+        <button>提交</button>
+    </p>
+</form>
+<script>
+    function isEmpty(){
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        if(email.length === 0 || password.length ===0){
+            alert('邮箱或密码不能为空');
+            return false;
+        }
+    }
+</script>
 </body>
 </html>
 
