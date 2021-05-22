037f37195444a041ceae9bd7c3bdec7246709076
方人胥 <37834565+fangrenxu@users.noreply.github.com>
2021-05-22 17:50:55 +0800

    函数的参数

--- function/demo2.php
+++ function/demo2.php
@@ -20,7 +20,15 @@ echo '<hr>';
 function sum1()
 {
     //func_num_args(): 返回实参的数量
-    return func_num_args();
+    //return func_num_args();
+
+    //return func_get_arg(0);//第一个实参
+    //return func_get_arg(1);//第二个实参
+
+    $a = func_get_arg(0);
+    $b = func_get_arg(1);
+
+    return $a + $b;
 }
 
-echo sum1(10,20);
+echo sum1(10,20,30,40,50);
