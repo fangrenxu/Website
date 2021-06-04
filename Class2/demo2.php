ff76dc92d28a0d5c2f87d0127e4049749da678ec
方人胥 <37834565+fangrenxu@users.noreply.github.com>
2021-06-04 14:38:02 +0800

    类常量的定义与访问

--- Class2/demo2.php
+++ Class2/demo2.php
@@ -1,5 +1,36 @@
 <?php
 //类常量的定义与访问
-define('SITE_NAME','PHP中文网');
-//宏替换
-echo SITE_NAME,'<br>';
+class Demo2
+{
+    const NATION = '中国';
+
+    public static $sex = '男';
+
+    private $name;
+
+    public function __construct($name)
+    {
+       $this->name = $name;
+    }
+
+    public function getInfo()
+    {
+        return $this->name.'的性别是：'.self::$sex.'，国籍是：'. self::NATION;
+    }
+}
+
+$obj = new Demo2('卢克');
+
+echo Demo2::$sex,'<br>';
+
+//访问类常量与访问类属性的方式是一样的
+echo Demo2::NATION,'<br>';
+
+echo $obj->getInfo(),'<br>';
+
+Demo2::$sex = '保密';
+echo Demo2::$sex,'<br>';
+
+//Demo2::NATION = '美国';
+//常量是不允许被更新的
+//echo Demo2::NATION,'<br>';
